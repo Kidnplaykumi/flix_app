a0d962bb4670486461a2ee0bd2e842b27108ca0d
Kidnplaykumi <elijah8512@yahoo.com>
2018-02-11 03:02:51 -0500

    Final

---
+++ Flix_app/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Flix_app
+//
+//  Created by Elijah Kumi on 2/11/18.
+//  Copyright © 2018 Elijah Kumi. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
